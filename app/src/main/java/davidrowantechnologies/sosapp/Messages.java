a4ea196350039b366450ac52da8cc87ae20253ae
Andrew <apm59@pitt.edu>
2017-02-10 23:10:07 -0500

    created message object

---
+++ app/src/main/java/davidrowantechnologies/sosapp/Messages.java
@@ -0,0 +1,28 @@
+package davidrowantechnologies.sosapp;
+
+/**
+ * Created by Andrew on 2/10/2017.
+ */
+
+public class Messages {
+    public static void main(String[] args)
+    {
+        /*public class Message
+        {
+            private String message;
+            public Message()
+            {
+
+            }
+            public String getMessage()
+            {
+                return message;
+            }
+            public void setMessage(String message)
+            {
+                message=new String(this.message);
+            }
+        }
+        */
+    }
+}
